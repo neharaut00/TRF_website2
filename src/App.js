8a1081c46f2cfb7ead326d03db41882ff9a266dc
neharaut00 <neharaut372@gmail.com>
2022-05-02 15:56:57 +0530

    ContactUs page done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Faculty from './Components/Faculty.js'
 import AboutUs from './Components/AboutUs.js'
 import Numbers from './Components/Numbers.js'
+import GetInTouch from './Components/GetInTouch.js'
 
 
 const App = () => {
@@ -9,7 +10,8 @@ const App = () => {
     <div style={{ margin: '0px', padding: '0px' }}>
       <Numbers />
       <AboutUs />
-      <Faculty />      
+      <Faculty />     
+      <GetInTouch />
       
     </div>
   )
