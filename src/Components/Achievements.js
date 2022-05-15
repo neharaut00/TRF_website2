e8cd1831b6c94dd997d134a1fa9704660ffc9bb6
neharaut00 <neharaut372@gmail.com>
2022-05-15 19:20:00 +0530

    changeYear state added

--- src/Components/Achievements.js
+++ src/Components/Achievements.js
@@ -1,22 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Achievements.css'
 import {Row, Col} from 'react-bootstrap'
 import A1 from '../assets/Achievements_images/A1.png'
-// import A21 from '../assets/Achievements_images/A2.1.png'
 import A2 from '../assets/Achievements_images/A2.png'
 import A3 from '../assets/Achievements_images/A3.png'
 import A4 from '../assets/Achievements_images/A4.png'
 import A5 from '../assets/Achievements_images/A5.png'
 import A61 from '../assets/Achievements_images/A6.1.png'
-import A62 from '../assets/Achievements_images/A6.2.png'
 import A7 from '../assets/Achievements_images/A7.png'
 import A81 from '../assets/Achievements_images/A8.1.png'
-import A82 from '../assets/Achievements_images/A8.2.png'
 
 function Achievements() {    
+    const [year, setYear] = useState("007");
+
+    const changeYear = () => {
+        console.log(window.scrollY)
+        if (window.scrollY > 3350) {
+            setYear("014");
+            // setYear(2009);
+        }
+        else if(window.scrollY > 3135) {
+            setYear("013");
+        }
+        else if(window.scrollY > 2648) {
+            setYear("012");
+        }
+        else if(window.scrollY > 1960){
+            setYear("011");
+        }
+        else if(window.scrollY > 1373){
+            setYear("010");
+        }
+        else if(window.scrollY > 916){
+            setYear("009");
+        }
+        else if(window.scrollY > 436){
+            setYear("008");
+        }       
+        else {
+            setYear("007");
+        }
+    }
+
+    window.addEventListener('scroll', changeYear);
+
   return (
       <div className="achievements">
-          <h1 className="year" style={{ 'position': "fixed",'backgroundColor':'transparent','display':'relative', "top": "35%", "left":"35%", "color":"white", "fontFamily":"Joyride"}}><span style={{'color':'black', 'textShadow': "white", 'backgroundColor':'transparent', 'textShadow':'1px 0 0 #fff, -1px 0 0 #fff, 0 1px 0 #fff, 0 -1px 0 #fff, 0.5px 0.5px #fff, -0.5px -0.5px 0 #fff, 0.5px -0.5px 0 #fff, -0.5px 0.5px 0 #fff'}}>2</span>007</h1>          
+          <h1 className="year" style={{ 'position': "fixed", 'backgroundColor': 'transparent', 'display': 'relative', "top": "35%", "left": "35%", "color": "white", "fontFamily": "Joyride" }}><span style={{ 'color': 'black', 'backgroundColor': 'transparent', 'textShadow': '1px 0 0 #fff, -1px 0 0 #fff, 0 1px 0 #fff, 0 -1px 0 #fff, 0.5px 0.5px #fff, -0.5px -0.5px 0 #fff, 0.5px -0.5px 0 #fff, -0.5px 0.5px 0 #fff' }}>2</span>{year}</h1>          
           <Row className="achievement-row">
               <Col>
               <img src={A1} alt="Achievements" />
