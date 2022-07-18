5ab4939b0399db3eba67cc2bd192ea7678daccac
neharaut00 <neharaut372@gmail.com>
2022-07-18 21:42:47 +0530

    countup on coming to the section

--- src/Components/HomePage/Numbers.js
+++ src/Components/HomePage/Numbers.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Row, Col } from "react-bootstrap";
 import CountUp from "react-countup";
 import "./Numbers.css";
+import VisibilitySensor from "react-visibility-sensor";
 
 function Numbers() {
   return (
@@ -18,40 +19,67 @@ function Numbers() {
       <div>
         <Row className="Numbers-row">
           <Col className="Numbers-col" xs={12} md={4}>
-            <div data-aos="fade-up" data-aos-once="true" className="Numbers">
+            <div className="Numbers">
               <h1>
-                <CountUp start={0} end={300} duration={4} />
+                <CountUp start={0} end={300} duration={3} redraw={true} />
+                {({ countUpRef, start }) => (
+                  <VisibilitySensor onChange={start} >
+                    <span ref={countUpRef} />
+                  </VisibilitySensor>
+                )}
                 <span>+</span>
               </h1>
             </div>
             <div>
-              <p data-aos="fade-up" className="Numbers-desc" data-aos-once="true">
+              <p
+                data-aos="fade-up"
+                className="Numbers-desc"
+                data-aos-once="true"
+              >
                 Club Members
               </p>
             </div>
           </Col>
           <Col className="Numbers-col" xs={12} md={4}>
-            <div data-aos="fade-up" data-aos-once="true" className="Numbers">
+            <div className="Numbers">
               <h1>
-                <CountUp start={0} end={50} duration={4} />
+                <CountUp start={0} end={50} duration={3} redraw={true} />
+                {({ countUpRef, start }) => (
+                  <VisibilitySensor onChange={start} delayedCall>
+                    <span ref={countUpRef} />
+                  </VisibilitySensor>
+                )}
                 <span>+</span>
               </h1>
             </div>
             <div>
-              <p data-aos="fade-up" className="Numbers-desc" data-aos-once="true">
+              <p
+                data-aos="fade-up"
+                className="Numbers-desc"
+                data-aos-once="true"
+              >
                 Team members
               </p>
             </div>
           </Col>
           <Col className="Numbers-col" xs={12} md={4}>
-            <div data-aos="fade-up" data-aos-once="true" className="Numbers">
+            <div className="Numbers">
               <h1>
-                <CountUp start={0} end={450} duration={4} />
+                <CountUp start={0} end={450} duration={3} redraw={true} />
+                {({ countUpRef, start }) => (
+                  <VisibilitySensor onChange={start} delayedCall>
+                    <span ref={countUpRef} />
+                  </VisibilitySensor>
+                )}
                 <span>+</span>
               </h1>
             </div>
             <div>
-              <p data-aos="fade-up" className="Numbers-desc" data-aos-once="true">
+              <p
+                data-aos="fade-up"
+                className="Numbers-desc"
+                data-aos-once="true"
+              >
                 Alumni
               </p>
             </div>
